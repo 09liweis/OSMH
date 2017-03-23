75cff2dd90fe79799013c8778a7d670b69c700df
hike10000miles <chenshen4009@gmail.com>
2017-03-23 15:38:26 -0400

    add alert models

---
+++ OSMH/Models/Alert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HumberWebDevProject.Models
+{
+	public class Alert
+	{
+		public enum Status
+		{
+			Inactive, Ongoing, Archived
+		}
+
+		public enum AlertClass
+		{
+			Fire, PoliceInvestigation, HazardousWeather, Unknown
+		}
+
+		public int AlertID { get; set; }
+		public string Title { get; set; }
+		public AlertClass Classification { get; set; }
+		public string Message { get; set; }
+		public Status AlertStatus { get; set; }
+		public string Publisher { get; set; }
+		public DateTime CreatingTime { get; set; }
+		public DateTime PublishingTime { get; set; }
+
+		public virtual AlertArchive AlertArchive { get; set; }
+	}
+}
---
+++ OSMH/Models/AlertAchive.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HumberWebDevProject.Models
+{
+	public class AlertArchive
+	{
+		[ForeignKey("Alert")]
+		public int AlertArchiveID { get; set; }
+		public DateTime ArchivedTime { get; set; }
+		// Admin Name should be AdminID once intergraded with other feature
+		public string AdminName { get; set; }
+
+		public virtual Alert Alert { get; set; }
+	}
+}
