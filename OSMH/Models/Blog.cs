0603dd81ad880684e51d8923b8bd4c15366766c9
Sam Li <Sam Li>
2017-03-23 10:11:18 -0400

    add jquery ui

--- OSMH/Models/Blog.cs
+++ OSMH/Models/Blog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,13 @@ namespace OSMH.Models
     public class Blog
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Content { get; set; }
         public bool Published { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime PublishDate { get; set; }
     }
 }
