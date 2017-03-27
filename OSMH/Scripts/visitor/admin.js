2d96086417056229db5e8448af03756cef8c280d
Yinan Bao <byn9826@gmail.com>
2017-03-27 17:17:04 -0400

    visitor reg

---
+++ OSMH/Scripts/visitor/admin.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        type: "POST",
+        url: "./readAdmin",
+        success: function (result) {
+            console.log(result);
+
+            var visitorLimit = result.visitorLimit;
+            visitorLimit.VisitorLimit_start = visitorLimit.VisitorLimit_start + ":00";
+            visitorLimit.VisitorLimit_end = visitorLimit.VisitorLimit_end + ":00";
+            if (!visitorLimit.VisitorLimit_date) {
+                var date = new Date();
+                var today = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+                visitorLimit.VisitorLimit_date = today;
+            }
+
+            var today = new Vue({
+                el: "#today",
+                data: {
+                    limitations: visitorLimit,
+                    registered: result.visitorRegs
+                }
+            })
+
+        }
+    });
+});
