6cc488ff867cfbc00048a5af62b790fb870d39b8
Sam <Sam@DESKTOP-QVKTKL6>
2017-03-29 16:24:48 -0400

    merge with master

---
+++ OSMH/Models/VisitorReg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OSMH.Models
+{
+    [Table("VisitorReg")]
+    public class VisitorReg
+    {
+        [Key]
+        public int VisitorReg_id { get; set; }
+        public Nullable<DateTime> VisitorReg_date { get; set; }
+        public string VisitorReg_code { get; set; }
+        public string VisitorReg_name { get; set; }
+    }
+}
---
+++ OSMH/Models/visitorAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OSMH.Models
+{
+    public class visitorAdmin
+    {
+        public VisitorLimit visitorLimit { get; set; }
+        public int visitorRegs { get; set; }
+    }
+}
