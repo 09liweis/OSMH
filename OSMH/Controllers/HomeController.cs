3a6b4b9436183a684374afc35e077990ae20b6cf
Sam <Sam@DESKTOP-QVKTKL6>
2017-04-26 23:12:39 -0400

    add homepage model to display data to homepage

--- OSMH/Controllers/HomeController.cs
+++ OSMH/Controllers/HomeController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OSMH.Models;
 
 namespace OSMH.Controllers
 {
     public class HomeController : Controller
     {
+        private OSMHDbContext db = new OSMHDbContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            Blog blog = db.blogs.Where(b => b.Published == true).OrderByDescending(b => b.PublishDate).First();
+            Testimonial test = db.Testimonials.Where(t => t.Approval == true).First();
+            Homepage homepage = new Homepage()
+            {
+                blog = blog,
+                testimonial = test
+            };
+            return View(homepage);
         }
     }
 }
